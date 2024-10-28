fea70df3d92e65c4e70bccd4b5bdfeb7176f596b
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2024-10-28 07:59:21 +0200

    updated the hang.js with a new approach

--- scripts/hang.js
+++ scripts/hang.js
@@ -1,28 +1,48 @@
-var bodyParts = [
-    'assets/head.svg',
-    'assets/body.svg',
-    'assets/left-hand.svg',
-    'assets/right-hand.svg',
-    'assets/left-leg.svg',
-    'assets/right-leg.svg'
-];
-const bodyPartClasses = [
-    'head',
-    'body',
-    'left-hand',
-    'right-hand',
-    'left-leg',
-    'right-leg'
-];
+// var bodyParts = [
+//     'assets/head.svg',
+//     'assets/body.svg',
+//     'assets/left-hand.svg',
+//     'assets/right-hand.svg',
+//     'assets/left-leg.svg',
+//     'assets/right-leg.svg'
+// ];
+// const bodyPartClasses = [
+//     'head',
+//     'body',
+//     'left-hand',
+//     'right-hand',
+//     'left-leg',
+//     'right-leg'
+// ];
 
+// function updateHangman() {
+//     var hangmanImage = document.getElementsByClassName('stand')[0];
+//     if (wrongGuesses < bodyParts.length) {
+//         hangmanImage.src = bodyParts[wrongGuesses];
+//         hangmanImage.className = 'stand';
+//         hangmanImage.classList.add(bodyPartClasses[wrongGuesses]);
+//     }
+// }
+
+var bodyParts = [
+    { src: 'assets/head.svg', className: 'head' }, // Make sure this path is correct
+    { src: 'assets/body.svg', className: 'body' },
+    { src: 'assets/left-hand.svg', className: 'left-hand' },
+    { src: 'assets/right-hand.svg', className: 'right-hand' },
+    { src: 'assets/left-leg.svg', className: 'left-leg' },
+    { src: 'assets/right-leg.svg', className: 'right-leg' }
+];
 function updateHangman() {
-    var hangmanImage = document.getElementsByClassName('stand')[0];
+    var hangContainer = document.getElementsByClassName('hang')[0];
+
     if (wrongGuesses < bodyParts.length) {
-        hangmanImage.src = bodyParts[wrongGuesses];
-        hangmanImage.className = 'stand'; // Reset to just the stand class
+        // Create a new image element for the current body part
+        var part = bodyParts[wrongGuesses];
+        var partImg = document.createElement('img');
+        partImg.src = part.src;
+        partImg.className = part.className;
         
-        // Add the new body part class
-        hangmanImage.classList.add(bodyPartClasses[wrongGuesses]);
+        // Add the new body part to the container
+        hangContainer.appendChild(partImg);
     }
 }
-
